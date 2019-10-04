b5e52f7bea7d2531cb90428a2731f07ce763354c
sergy-git <sergy.stepura@gmail.com>
2019-10-04 12:32:10 +0300

    TAG: Random strategy v0.0

--- catch-me.py
+++ catch-me.py
@@ -1,4 +1,9 @@
+from q_tools import Plot
 from random import choice
+from time import sleep
+from math import exp, log
+from typing import List
+
 
 def new_position(position, action):
     return position[0] + action[0], position[1] + action[1]
@@ -6,35 +11,37 @@ def new_position(position, action):
 
 class Actor:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.init_x = x
+        self.init_y = y
+        self.x = self.init_x
+        self.y = self.init_y
         self.x_prev = None
         self.y_prev = None
         self.action = None
-        self.test_action = None
         self.type = None
 
-    def reset(self, x, y):
-        self.x = x
-        self.y = y
+    def reset(self):
+        self.x = self.init_x
+        self.y = self.init_y
         self.x_prev = None
         self.y_prev = None
         self.action = None
-        self.test_action = None
 
     def get_pos(self):
         return self.x, self.y
 
-    def move(self):
-        self.action = self.test_action
-        self.x_prev = self.x
-        self.y_prev = self.y
-        self.x += self.action[0]
-        self.y += self.action[1]
-
-    def valid_move(self, action, rules):
-        self.test_action = action
-        return rules(new_position(self.get_pos(), self.test_action))
+    def move(self, strategy, check_rule):
+        valid = False
+        while not valid:
+            action = strategy()
+            x_new, y_new = new_position(self.get_pos(), action)
+            valid = check_rule((x_new, y_new))
+            if valid:
+                self.action = action
+                self.x_prev = self.x
+                self.y_prev = self.y
+                self.x = x_new
+                self.y = y_new
 
     def transition(self):
         return (self.x_prev, self.y_prev), self.action, (self.x, self.y)
@@ -77,22 +84,71 @@ class Enemy(Actor):
 
 
 class World:
+    player: Actor
+    enemies: List[Actor]
+    actors: List[Actor]
+
     def __init__(self, model_strategy, n=7, m=7):
+        self.actions = ((0, 0), (0, 1), (1, 0), (0, -1), (-1, 0))
         self.model_strategy = model_strategy
         self.n = n
         self.m = m
-        self.actions = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        self.actors = [Player(1, 1), Enemy(3, 1), Enemy(1, 3), Enemy(3, 3)]
-        self.board = SimpleBoard(5, 5, self.actors)
+        self.player = Player(1, 1)
+        self.enemies = [Enemy(4, 1), Enemy(1, 4), Enemy(4, 4)]
+        self.actors = [self.player] + self.enemies
+        self.board = SimpleBoard(self.n, self.m, self.actors)
         self.step_count = 0
-        self.game_over = None
 
-    def game_over(self, player, enemys):
-        x, y = player.get_pos()
-        for enemy in enemys:
-            if enemy
+    def reset(self):
+        self.step_count = 0
+        for actor in self.actors:
+            actor.reset()
+
+    def game_over(self):
+        return any(map(lambda enemy: enemy.get_pos() == self.player.get_pos(), self.enemies))
+
+    def rule(self, position):
+        x, y = position
+        return 0 <= x < self.n and 0 <= y < self.m
+
+    def strategy(self):
+        return choice(self.actions)
+
+    def draw(self):
+        sleep(1)
+        self.board.draw()
+        print('Steps:', self.step_count)
+
+    def play(self, silent=False):
+        self.step_count += 1
+        for actor in self.actors:
+            actor.move(self.strategy, self.rule)
+        if not silent:
+            self.draw()
+        return not self.game_over()
+
+
+class Epsilon:
+    def __init__(self, max_epochs):
+        self.eps_start = exp(log(1e-6)/max_epochs)
+        self.eps = 1
+
+    def step(self):
+        self.eps *= self.eps_start
+        return self.eps if self.eps > 1e-5 else 0
 
 
 if __name__ == '__main__':
-    world = World(choice)
+    MAX_EPOCHS = 100
+    ALPHA = 0.5
+    GAMMA = 0.9
 
+    plot = Plot(rolling={'method': 'mean', 'N': 10})
+    eps = Epsilon(MAX_EPOCHS)
+
+    world = World(None)
+    for _ in range(MAX_EPOCHS):
+        while world.play(silent=True):
+            pass
+        plot.update(world.step_count)
+        world.reset()
--- q_tools.py
+++ q_tools.py
@@ -1,20 +1,35 @@
-from matplotlib import pyplot as plt
+from matplotlib import pyplot
+from statistics import mean
 
 
 class Plot:
-    def __init__(self):
+    def __init__(self, rolling=None):
         self.line = []
+        if rolling is not None:
+            self.method = rolling['method']
+            self.N = rolling['N']
+            self.roll = []
+        else:
+            self.method = None
+            self.roll = None
+
+    def roll_append(self):
+        if self.method is 'mean':
+            self.roll.append(None if len(self.line) < self.N else mean(self.line[-self.N:]))
 
     def update(self, value, silent=False):
         self.line.append(value)
+        self.roll_append()
         if not silent:
             self.plot()
 
     def plot(self):
-        plt.figure(0)
-        plt.clf()
-        plt.title('Training...')
-        plt.xlabel('Epoch')
-        plt.ylabel('Duration')
-        plt.plot(self.line)
-        plt.pause(0.00001)  # pause a bit so that plots are updated
+        pyplot.figure(0)
+        pyplot.clf()
+        pyplot.title('Training...')
+        pyplot.xlabel('Epoch')
+        pyplot.ylabel('Duration')
+        pyplot.plot(self.line)
+        if self.roll is not None:
+            pyplot.plot(self.roll)
+        pyplot.pause(0.00001)  # pause a bit so that plots are updated
